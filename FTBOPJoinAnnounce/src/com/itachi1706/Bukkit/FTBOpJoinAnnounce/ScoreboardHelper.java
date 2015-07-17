f2628de034d8ed8b7c7b50cdffa059c0b59ca5d1
itachi1706 <kennethsohyq@hotmail.com>
2015-07-17 18:11:00 +0800

    Migrated FTBOPJoinAnnounce

---
+++ FTBOPJoinAnnounce/src/com/itachi1706/Bukkit/FTBOpJoinAnnounce/ScoreboardHelper.java
@@ -0,0 +1,50 @@
+package com.itachi1706.Bukkit.FTBOpJoinAnnounce;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
+import org.bukkit.scoreboard.Team;
+
+/**
+ * Created by Kenneth on 17/7/2015.
+ * for FTBOPJoinAnnounce in com.itachi1706.Bukkit.FTBOpJoinAnnounce
+ */
+public class ScoreboardHelper {
+
+    static ScoreboardManager manager = Bukkit.getScoreboardManager();
+    static Scoreboard board = manager.getMainScoreboard();
+    static Team regular = board.getTeam("Regulars");
+    static Team mod = board.getTeam("Moderators");
+
+    public static void ensureTeamsAvailable(){
+        if (board.getTeam("Moderators") == null){
+            board.registerNewTeam("Moderators");
+        }
+
+        if (board.getTeam("Regulars") == null){
+            board.registerNewTeam("Regulars");
+        }
+    }
+
+    public static boolean checkIfRegular(Player p){
+        if (regular.getSize() == 0){
+            return false;
+        }
+        if (regular.hasPlayer(p.getPlayer())){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean checkIfMod(Player p){
+        if (mod.getSize() == 0){
+            return false;
+        }
+        if (mod.hasPlayer(p.getPlayer())){
+            return true;
+        }
+        return false;
+    }
+
+}
